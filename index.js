438059a20cc7d67d7a068fd42df3fd3c9d2ed634
AlvGreat <60948906+AlvGreat@users.noreply.github.com>
2020-12-07 20:11:33 -0800

    Updated ping/help commands
    Cleaned up the code a bit

--- index.js
+++ index.js
@@ -8,34 +8,39 @@ client.once('ready', () => {
 });
 
 client.on('message', async message => {
-    //this will check if a message starts with the bot prefix or if the message sender is a Discord bot themselves
+    // if cabot is pinged:
+    if (message.mentions.users.first() && message.mentions.users.first().id === "742440632236113961") {
+        message.channel.send("Hello! I'm the official bot of the Underdogs Cup server. My prefix is `!` and you can type !help to see my list of commands.");
+    }
+
+    // this will check if a message starts with the bot prefix or if the message sender is a Discord bot themselves
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    //will take messages from Discord and check if they match the commands below
+    // will take messages from Discord and check if they match the commands below
     const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
 
     if (command === 'cabpun') {
         const puns = [
-    "What does he do when he’s disappointed?\nCaboozled sighs",
-    "What's Cab's favorite mathematical symbol?\nCaboozled_pi.",
-    "What's gonna happen when Cab misdrops?\nHe'll Caboozled_die.",
-    "What do his followers/underlings call him?\nCaboozled sen-pie.",
-    "What do Cab's fans say when he brutally murders them (in Tetris)?\nCaboozled, why?!",
-    "What does Cab do when he's sleepy?\nCaboozled lies (in bed).",
-    "What does Cab do when he gets tagged?\nCaboozled replies.",
-    "What does Cab do when he loses his X rank?\nCaboozled cries.",
-    "What does Cab do at the store?\nCaboozled buys.",
-    "How does Cab feel when he gets disconnected?\nConfuzled_pie.",
-    "What does Cab do in zero gravity?\nCaboozled flies.",
-    "What kind of bread does Cab eat?\nCaboozled rye.",
-    "What does Cab serve at a fast food restaurant?\nCaboozled fries.",
-    "How does Cab act when he's embarrassed?\nCaboozled shie.",
-    "What's Cab's gender?\nCaboozled guy.",
-    "What does Cab do to you no one?\nCaboozled spies.",
-    "What happens when Cab goes 6-6 against another player?\nCaboozled ties.",
-    "When does Cab do when he's in a tourney?\nCaboozled vies.",
-    "What does cab say when hes tired of bad puns?\nCaboozled_bye."
+            "What does he do when he’s disappointed?\nCaboozled sighs",
+            "What's Cab's favorite mathematical symbol?\nCaboozled_pi.",
+            "What's gonna happen when Cab misdrops?\nHe'll Caboozled_die.",
+            "What do his followers/underlings call him?\nCaboozled sen-pie.",
+            "What do Cab's fans say when he brutally murders them (in Tetris)?\nCaboozled, why?!",
+            "What does Cab do when he's sleepy?\nCaboozled lies (in bed).",
+            "What does Cab do when he gets tagged?\nCaboozled replies.",
+            "What does Cab do when he loses his X rank?\nCaboozled cries.",
+            "What does Cab do at the store?\nCaboozled buys.",
+            "How does Cab feel when he gets disconnected?\nConfuzled_pie.",
+            "What does Cab do in zero gravity?\nCaboozled flies.",
+            "What kind of bread does Cab eat?\nCaboozled rye.",
+            "What does Cab serve at a fast food restaurant?\nCaboozled fries.",
+            "How does Cab act when he's embarrassed?\nCaboozled shie.",
+            "What's Cab's gender?\nCaboozled guy.",
+            "What does Cab do to you no one?\nCaboozled spies.",
+            "What happens when Cab goes 6-6 against another player?\nCaboozled ties.",
+            "When does Cab do when he's in a tourney?\nCaboozled vies.",
+            "What does cab say when hes tired of bad puns?\nCaboozled_bye."
         ]
 
         const randomPun = Math.floor((Math.random() * puns.length));
@@ -56,17 +61,20 @@ client.on('message', async message => {
         sentEmbed.react('733831723187568690')
         sentEmbed.react('📹')
     }
+    
+    else if (command === "help") {
+        message.channel.send("**Commands**:\n!help, !ping, !cabpun, !roles, !nickname, !togglecoach [coaches only], and !queue [coaches only]");
+    }
 
     else if (command === "ping") {
-        message.channel.send("Pong, I'm currently online! My commands are: !ping, !cabpun, !roles, and !togglecoach")
+        message.channel.send("Pong, I'm currently online! My prefix is `!` and you can type !help to see my list of commands.")
     }
-    else if (command === "togglecoach") {
-      let coachRole = "721831112103428160";
-      let activeCoachRole = "769805724930277416";
-      let retiredCoachRole = "771925290145677322"
 
-      //var coachRole = "769812406758539285";
-      //var activeCoachRole = "647172876406882329";
+    else if (command === "togglecoach") {
+      const coachRole = "721831112103428160";
+      const activeCoachRole = "769805724930277416";
+      const retiredCoachRole = "771925290145677322"
+
       if (message.member.roles.cache.has(coachRole)){
         if (message.member.roles.cache.has(activeCoachRole)) {
           message.member.roles.remove(activeCoachRole);
@@ -82,21 +90,23 @@ client.on('message', async message => {
         message.reply("Set as active coach. Welcome back!");
       }
 
-    } else if (command == "nickname") {
+    } 
+    
+    else if (command == "nickname") {
       let participantRole = "785609922654109796";
       if (message.member.roles.cache.has(participantRole)) {
         message.reply("Tournament participants cannot change their nickname to make finding your opponent easier on tournament day.")
       } else {
         message.member.setNickname(args.join(" "))
         .then(() => message.reply("nickname set"))
-        .catch(() => {message.reply("I can't change your nickname, probably because of permissions")});
+        .catch(() => {message.reply("I can't change your nickname, probably because of permissions. Please contact a staff member if you need help.")});
       }
-    } else if (command == "queue") {
-
-      let coachRole = "721831112103428160";
-      let serverID = "718603683624910941"
-      let channelID = "773575708613935104"
-
+    } 
+    
+    else if (command == "queue") {
+      const coachRole = "721831112103428160";
+      const serverID = "718603683624910941"
+      const channelID = "773575708613935104"
 
       if (message.member.roles.cache.has(coachRole)){
         client.channels.fetch(channelID).then((queueChannel) => {
@@ -114,7 +124,7 @@ client.on('message', async message => {
     }
 });
 
-//UC server id, for adding a role after adding reaction
+// UC server id, for adding a role after adding reaction
 client.on("messageReactionAdd", async (reaction, user)=>{
     try {
         if(reaction.message.partial) await reaction.message.fetch();
@@ -144,7 +154,7 @@ client.on("messageReactionAdd", async (reaction, user)=>{
     }
 });
 
-//UC server id, for removing a role after removal of reaction
+// UC server id, for removing a role after removal of reaction
 client.on("messageReactionRemove", async (reaction, user)=>{
     try{
         if(reaction.message.partial) await reaction.message.fetch();
@@ -173,6 +183,5 @@ client.on("messageReactionRemove", async (reaction, user)=>{
         console.log(e);
     }
 });
-
 
 client.login(process.env.BOT_TOKEN);
